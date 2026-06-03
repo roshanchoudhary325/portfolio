72c4b6b7a5658638b56bfaaacea5c2ab7224b33e
roshanchoudhary325 <156527481+roshanchoudhary325@users.noreply.github.com>
2026-06-03 17:00:27 +0530

    fixing error

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',S
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
